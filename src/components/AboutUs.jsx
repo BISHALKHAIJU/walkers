686cb25004b34a15cb2068213459f26dedac4dc6
BISHALKHAIJU <140307492+BISHALKHAIJU@users.noreply.github.com>
2025-12-28 15:52:15 +0545

    updated

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -13,25 +13,26 @@ const AboutUs = () => {
     >
       <div className="relative max-w-[1000px] mx-auto text-center py-4 mt-20 bg-[#FFF8F8] rounded-2xl">
         <img
-          src="aboutBg.jpg"
+          src="about.jpg"
           alt=""
-          className="absolute top-0 left-0 w-full h-full object-cover z-[10] opacity-60 rounded-2xl"
+          className="absolute top-0 left-0 w-full h-full object-cover z-[10] opacity-40 rounded-2xl"
         />
 
         <div className="relative px-4 z-10 py-10 md:py-12 text-left md:text-center">
-          <span className="font-[Outfit] font-medium text-[10px] text-[#1976D2]">
+          <span className="font-[Outfit] font-bold text-[14px] text-[#1976D2]">
             ABOUT US
           </span>
 
-          <h1 className="font-[Outfit] font-medium text-[40px] text-[#3d3d3d]">
-        Walkers Hive
+          <h1 className="font-[Outfit] font-medium text-[40px] text-[#000000]">
+            Walkers Hive
           </h1>
 
-          <p className="text-sm font-[Outfit] text-[#666666] max-w-[300px] md:max-w-[596px] md:mx-auto mt-4 leading-6">
-        Walkers Hive is a next-gen software company built at the intersection
-        of technology, creativity, and impact. We're a team of engineers,
-        designers, data scientists, and problem-solvers obsessed with building
-        software that scales, systems that think, and experiences that inspire.
+          <p className="text-sm text-start font-[Outfit] text-[#000000] max-w-[300px] md:max-w-[596px] md:mx-auto mt-4 leading-6">
+            Walkers Hive is a next-gen software company built at the
+            intersection of technology, creativity, and impact. We're a team of
+            engineers, designers, data scientists, and problem-solvers obsessed
+            with building software that scales, systems that think, and
+            experiences that inspire.
           </p>
         </div>
       </div>
